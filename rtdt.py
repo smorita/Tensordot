963dd8502fc2b08422134250ad38829d2c5bed14
Satoshi Morita <morita@issp.u-tokyo.ac.jp>
2015-12-10 17:58:28 +0900

    Fixed the usage message of rtdt.py.

--- rtdt.py
+++ rtdt.py
@@ -4,7 +4,7 @@ import tdt
 
 if __name__ == "__main__":
     if len(sys.argv)<2:
-        sys.exit("Usage: ./tdtr.py input_file [iteration]")
+        sys.exit("Usage: ./rtdt.py input_file [iteration]")
 
     iteration = 0 if len(sys.argv)<3 else int(sys.argv[2])
     tdt.main(True,iteration)
