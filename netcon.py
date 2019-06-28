bb8fe79e5007767eef513824aa534681ffefa883
infrub <infrub@gmail.com>
2019-06-29 01:47:38 +0900

    Clear +1 -> p1

--- netcon.py
+++ netcon.py
@@ -67,15 +67,18 @@ class NetconClass:
             logging.info("netcon: searching with mu_cap={0:.6e}".format(mu_cap))
             mu_next = sys.float_info.max
             for c in range(1,n):
+                cp1 = c+1
                 for d1 in range((c+1)//2):
-                    d2 = c-d1-1
-                    n1 = len(tensors_of_size[d1+1])
-                    n2 = len(tensors_of_size[d2+1])
+                    d1p1 = d1+1
+                    d2 = c-d1p1
+                    d2p1 = d2+1
+                    n1 = len(tensors_of_size[d1p1])
+                    n2 = len(tensors_of_size[d2p1])
                     for i1 in range(n1):
-                        i2_start = i1+1 if d1==d2 else 0
+                        i2_start = i1+1 if d1p1==d2p1 else 0
                         for i2 in range(i2_start, n2):
-                            t1 = tensors_of_size[d1+1][i1]
-                            t2 = tensors_of_size[d2+1][i2]
+                            t1 = tensors_of_size[d1p1][i1]
+                            t2 = tensors_of_size[d2p1][i2]
 
                             if self._is_disjoint(t1,t2): continue
                             if self._is_overlap(t1,t2): continue
@@ -87,13 +90,13 @@ class NetconClass:
                             if (mu > mu_0) and (mu <= mu_cap):
                                 t_new = self._contract(t1,t2)
                                 is_find = False
-                                for i,t_old in enumerate(tensors_of_size[c+1]):
+                                for i,t_old in enumerate(tensors_of_size[cp1]):
                                     if t_new.bits == t_old.bits:
                                         if t_new.cost < t_old.cost:
-                                            tensors_of_size[c+1][i] = t_new
+                                            tensors_of_size[cp1][i] = t_new
                                         is_find = True
                                         break
-                                if not is_find: tensors_of_size[c+1].append(t_new)
+                                if not is_find: tensors_of_size[cp1].append(t_new)
             mu_old = mu_cap
             mu_cap = max(mu_next, mu_cap*xi_min)
             for s in tensors_of_size:
