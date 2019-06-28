f809cd3b5ae7d8774efefd1d773f4c2f98bda9a5
infrub <infrub@gmail.com>
2019-06-29 01:55:29 +0900

    Yuusen ovelap

--- netcon.py
+++ netcon.py
@@ -72,8 +72,8 @@ class NetconClass:
                     for i1, t1 in enumerate(tensors_of_size[d1]):
                         i2_start = i1+1 if d1==d2 else 0
                         for t2 in tensors_of_size[d2][i2_start:]:
-                            if self._is_disjoint(t1,t2): continue
                             if self._is_overlap(t1,t2): continue
+                            if self._is_disjoint(t1,t2): continue
 
                             mu = self._get_cost(t1,t2)
                             mu_0 = 0.0 if (t1.is_new or t2.is_new) else mu_old
